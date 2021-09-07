7a4314274e5241ba7017c46f17925d6c9bc88a12
gegius <glebushka.ageev@gmail.com>
2021-09-07 19:26:14 +0500

    Тесты

--- src/test/NCubeTest.java
+++ src/test/NCubeTest.java
@@ -9,7 +9,7 @@ public class NCubeTest {
     public void countVertexesTest(){
         var func = "01001010010010010100101101111011";
         var cube = new NCube(func);
-        Assert.assertEquals(14, cube.getVertexes().size());
+        Assert.assertEquals(16, cube.getVertexes().size());
         func = "0100101001001001010010110111101101001010010010010100101101111011";
         cube = new NCube(func);
         Assert.assertEquals(32, cube.getVertexes().size());
